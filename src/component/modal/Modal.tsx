a0c263216e7cc7f3a2e89bb1bcb9315db63d8896
jxxnixx <dkepffk10@gmail.com>
2024-10-06 17:35:32 +0900

    Fix: Conditional Styling for Modal

--- src/component/modal/Modal.tsx
+++ src/component/modal/Modal.tsx
@@ -3,17 +3,20 @@
 import React, { useEffect } from 'react'
 import { createPortal } from 'react-dom'
 
+import { clc } from '@/utils/classComposer'
+
 import styles from './modal.module.scss'
 
 interface ModalProps {
 	modalId: string
 	children: React.ReactNode
 	onClose: () => void
+	fullPage?: boolean
 }
 
 const MODAL_PREFIX = 'modal_'
 
-const Modal = ({ modalId, children, onClose }: ModalProps) => {
+const Modal = ({ modalId, children, onClose, fullPage }: ModalProps) => {
 	useEffect(() => {
 		document.body.classList.add(styles.fix)
 		return () => document.body.classList.remove(styles.fix)
@@ -22,7 +25,7 @@ const Modal = ({ modalId, children, onClose }: ModalProps) => {
 	const ModalContent = () => (
 		<div
 			id={`${MODAL_PREFIX}${modalId}`}
-			className={styles.modal}
+			className={clc(styles.modal, fullPage ? styles.fullModal : styles.partModal)}
 			onClick={(e) => {
 				if (e.currentTarget === e.target) {
 					onClose()
